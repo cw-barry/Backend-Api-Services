606a2dbe1b144b8316248566f1f3bd9f79e012ff
cw-barry <barry@clarusway.com>
2022-11-01 15:23:26 -0400

    update password reset

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from .views import RegisterView #, ListUserView
+from dj_rest_auth.views import PasswordResetConfirmView
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+
+    path('password/reset/confirm', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("", include('dj_rest_auth.urls')),
     path('register/', RegisterView.as_view(), name="register"),
     # path('list/', ListUserView.as_view(), name="list"),
