80d99a142415d9f631757cd4d6cab23b9cbe87e9
cw-barry <barry@clarusway.com>
2022-11-01 14:36:34 -0400

    add static urls

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls')),
@@ -26,3 +29,6 @@ urlpatterns = [
     path('account/', include('account.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
