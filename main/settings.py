bd1fa475c5558aea4b465a42ec1bb37c629fc506
cw-barry <barry@clarusway.com>
2022-11-03 16:15:40 -0400

    update taskrate app

--- main/settings.py
+++ main/settings.py
@@ -54,7 +54,8 @@ INSTALLED_APPS = [
     "products",
     "account",
     "images",
-    "blog"
+    "blog",
+    "taskrater"
 
 ]
 
--- taskrater/models.py
+++ taskrater/models.py
@@ -5,7 +5,7 @@ class RateData(models.Model):
     task = models.CharField(max_length = 60, blank=True, null=True)
     query= models.CharField(max_length = 100)
     rate=models.SmallIntegerField()
-    job_date = models.DateTimeField(auto_add_now=True)
+    job_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.query
