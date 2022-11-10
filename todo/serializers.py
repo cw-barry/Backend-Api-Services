a62cdc0bf0c7dc3176b121ab398f6d7c672943cb
cw-barry <barry@clarusway.com>
2022-11-09 21:21:42 -0500

    update serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
     class Meta:
         model = Todo
-        fields = ("id", "title","status")
+        fields = ("id", "title","status", "user")
