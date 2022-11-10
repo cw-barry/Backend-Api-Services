e075013fc2757e59896ba0d52e9ff332a70ae5df
cw-barry <barry@clarusway.com>
2022-11-09 21:26:35 -0500

    update permission

--- todo/views.py
+++ todo/views.py
@@ -7,16 +7,16 @@ from .permissions import IsOwner
 # Create your views here.
 
 class TodoView(ModelViewSet):
-    # permission_classes = [IsAuthenticated, IsOwner]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated, IsOwner]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
 
-    # def get_queryset(self):
-    #     if self.request.user.is_staff:
-    #         return super().get_queryset()
-    #     else:
-    #         return Todo.objects.filter(user = self.request.user)
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return super().get_queryset()
+        else:
+            return Todo.objects.filter(user = self.request.user)
