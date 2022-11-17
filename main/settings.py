1cc426a062413ec0e88746d59dfe638762857b4d
cw-barry <barry@clarusway.com>
2022-11-17 17:12:11 -0500

    update settings

--- main/settings.py
+++ main/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = ['https://*.fullstack.clarusway.com']
 CORS_ALLOW_CREDENTIALS = True
 CORS_EXPOSE_HEADERS = (
     'Access-Control-Allow-Origin: *',
@@ -106,7 +107,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / (os.environ.get('DB') if os.environ.get('DB') else 'db.sqlite3'),
     }
 }
 
