391777987d53fd3bfee4452c69268c8acafe4dae
cw-barry <barry@clarusway.com>
2024-10-14 20:41:28 -0400

    add pagination

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
 class CategoryView(ModelViewSet):
@@ -14,13 +15,24 @@ class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     permission_classes = [IsAdminOrReadonly]
 
+class CustomPagination(PageNumberPagination):
+    page_size = 10  # Default page size
+    page_size_query_param = 'limit'  # Query parameter to specify the number of items per page
+    max_page_size = 500  # Maximum page size limit
+
 class ProductView(ModelViewSet):
     serializer_class = ProductsSerializer
     queryset = Products.objects.all()
     # permission_classes = [IsAdminOrReadonly]
     filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
-    search_fields=['title']
+    search_fields = ['title']
     filterset_fields = ['category']
+    pagination_class = CustomPagination  # Use the custom pagination class
+
+    def list(self, request, *args, **kwargs):
+        # Call the original list method
+        response = super().list(request, *args, **kwargs)
+        return Response(response.data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def bulk_create_api(request):
